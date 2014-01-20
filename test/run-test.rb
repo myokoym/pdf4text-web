0352e0fb24d5bdf26a7f5b6cdb573b812955dadb
Masafumi Yokoyama <myokoym@gmail.com>
2014-01-21 00:30:19 +0900

    test: exclude files except "test/"

--- test/run-test.rb
+++ test/run-test.rb
@@ -6,6 +6,7 @@ require "test/unit/rr"
 require "rack/test"
 
 base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-$LOAD_PATH.unshift(File.join(base_dir, "test"))
+test_dir = File.join(base_dir, "test")
+$LOAD_PATH.unshift(test_dir)
 
-exit Test::Unit::AutoRunner.run(true)
+exit Test::Unit::AutoRunner.run(true, test_dir)
